c47e5db673a95d4c377ee4f643ae7e5ff9cfb1c0
unknown <dev-patidar17>
2023-03-19 12:37:38 +0530

    first commit

---
+++ src/main/java/com/example/chatone/ChatoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chatone/ChatoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
